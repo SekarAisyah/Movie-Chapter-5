72a09cec9c91abafb3818892ac4b9b3aa6d605c1
SekarAisyah <sekarrisma@apps.ipb.ac.id>
2023-05-15 00:57:43 +0700

    Login,Register,Home,Detail Film,Update Profile

--- app/src/main/java/com/example/m/view/HomeFragment.kt
+++ app/src/main/java/com/example/m/view/HomeFragment.kt
@@ -21,11 +21,15 @@ class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
     lateinit var firebaseAuth:FirebaseAuth
     lateinit var sharedPreferences: SharedPreferences
+
+
+    //mendefinisikan override function onCreate untuk melakukan beberapa konfigurasi sebelum fragment ditampilkan, seperti memanggil setHasOptionsMenu(true) untuk menampilkan menu pada toolbar.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
     }
 
+    //mendefinisikan override function onCreateView untuk mengembalikan tampilan layout fragment.
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,29 +40,49 @@ class HomeFragment : Fragment() {
 
     }
 
+    //mendefinisikan override function onViewCreated yang akan dipanggil setelah tampilan fragment dibuat, untuk melakukan inisialisasi UI dan logic pada tampilan.
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //menetapkan toolbar yang ada pada layout sebagai ActionBar.
         (activity as AppCompatActivity).setSupportActionBar(binding.tbHome)
 
         firebaseAuth = FirebaseAuth.getInstance()
+        //mengecek apakah user sudah login atau belum, jika belum, maka akan diarahkan ke halaman login.
         if(firebaseAuth.currentUser == null){
             findNavController().navigate(R.id.action_homeFragment_to_loginFragment2)
         }
 
+        //menginisialisasi instance dari SharedPreferences.
+        //Dalam kode ini, requireContext() digunakan untuk mendapatkan konteks dari fragment saat ini dan kemudian digunakan untuk memanggil metode
+        // getSharedPreferences() dengan parameter "dataUser" dan MODE_PRIVATE. Sehingga, objek SharedPreferences dengan nama "dataUser"
+        // dan mode akses MODE_PRIVATE akan dibuat dan disimpan dalam variabel sharedPreferences.
+        // dan mode akses MODE_PRIVATE akan dibuat dan disimpan dalam variabel sharedPreferences.
         sharedPreferences = requireContext().getSharedPreferences("dataUser", Context.MODE_PRIVATE)
 
+        // mengambil data user yang sudah login dari SharedPreferences.
         var getUser = sharedPreferences.getString("user", "")
         binding.textJudul.text = "Welcome, $getUser"
 
+        //menambahkan event click pada tombol profil untuk menavigasikan ke halaman profil.
         binding.ivIcprofile.setOnClickListener {
             var addUser = sharedPreferences.edit()
             addUser.putString("user", getUser)
             addUser.apply()
             findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
         }
+
+        //menginisialisasi instance dari MovieViewModel.
         val viewModelFilm = ViewModelProvider(this).get(MovieViewModel::class.java)
+
+        //memanggil method callMovie() yang akan memuat daftar film dari server menggunakan Retrofit.
         viewModelFilm.callMovie()
+
+        //mengamati perubahan pada liveDataMovie pada MovieViewModel dan menampilkan daftar film pada RecyclerView jika ada perubahan pada data.
         viewModelFilm.liveDataMovie.observe(viewLifecycleOwner, Observer {
+
+            //Jika it tidak null, maka RecyclerView rvListFilm akan diatur layout managernya menjadi LinearLayoutManager dengan orientasi vertical
+            // dan diatur adapternya dengan data yang berasal dari it yang merupakan hasil dari API call. Jadi, ketika liveDataMovie pada ViewModel
+            // berubah nilainya, maka RecyclerView akan secara otomatis diperbarui dengan data terbaru.
             if (it != null) {
                 binding.rvListFilm.layoutManager =
                     LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
--- app/src/main/java/com/example/m/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/example/m/viewmodel/MovieViewModel.kt
@@ -11,10 +11,16 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MovieViewModel : ViewModel() {
+
+    //Membuat sebuah variabel liveDataMovie dengan tipe MutableLiveData<List<PopularMovieItem>?> yang nantinya akan berisi data film populer.
     var liveDataMovie: MutableLiveData<List<PopularMovieItem>?> = MutableLiveData()
 
+    // Membuat fungsi callMovie yang akan memanggil endpoint untuk mendapatkan data film populer dari server.
     fun callMovie() {
+        //Menggunakan ApiClient untuk melakukan koneksi ke server dan memanggil method getMoviePopular().
+        //Menggunakan enqueue() untuk menjalankan permintaan secara asynchronous dan memberikan Callback untuk menangani hasil respon.
         ApiClient.instance.getMoviePopular().enqueue(object : Callback<PopularMovieResponse> {
+            //onResponse() akan dipanggil ketika respon dari server diterima dengan berhasil, dan akan memperbarui nilai liveDataMovie dengan hasil response dari server.
             override fun onResponse(
                 call: Call<PopularMovieResponse>,
                 response: Response<PopularMovieResponse>
@@ -26,6 +32,7 @@ class MovieViewModel : ViewModel() {
                 }
             }
 
+            //onFailure() akan dipanggil ketika permintaan gagal karena alasan seperti jaringan atau kesalahan server, dan akan memperbarui nilai liveDataMovie menjadi null
             override fun onFailure(call: Call<PopularMovieResponse>, t: Throwable) {
                 liveDataMovie.postValue(null)
             }
